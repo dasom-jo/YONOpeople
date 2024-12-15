5a42041e93c30bd486c16b1569c5db6e73630d51
Turbobot <turbobot@vercel.com>
2024-12-15 17:16:14 +0900

    feat(create-turbo): apply official-starter transform

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    roots: ['<rootDir>/shared/__tests__', '<rootDir>/apps/web', '<rootDir>/apps/mobile'],
-    moduleNameMapper: {
-      '^@shared/(.*)$': '<rootDir>/shared/$1',
-    },
-  };
--- shared/__tests__/hooks.test.ts
+++
@@ -1,5 +0,0 @@
-describe('hooks test suite', () => {
-    it('should pass a basic test', () => {
-      expect(true).toBe(true);
-    });
-  });
