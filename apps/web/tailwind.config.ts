7e0360e1f8c616dea3a5647265c596ff3b776cbe
jodasom <jodasom0216@gmail.com>
2024-12-17 19:23:52 +0900

    feat:테일윈드설치및 공통버튼 완성

--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -1,18 +1,24 @@
-import type { Config } from "tailwindcss";
-
-export default {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./packages/ui/**/*.{js,ts,jsx,tsx}",
+    "../../packages/ui/**/*.{js,ts,jsx,tsx}", //\    "../../packages/ui/**/*.{js,ts,jsx,tsx}", /
+
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: '#DFF5FC',
+        clickbtn:"#0083AF",
+        basicbtn:'#71C4E0',
+        clickgary:"D9D9D9",
+        basicgray:"#F6F5F5"
       },
     },
   },
   plugins: [],
-} satisfies Config;
+};
