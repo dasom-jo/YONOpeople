cbb91f9d14097c7678292be80d832d11f93152bf
jodasom <jodasom0216@gmail.com>
2024-12-17 19:57:52 +0900

    feat: 카카오 아이디 공통 레이아웃  완성성

---
+++ packages/ui/src/KakaoIcon.tsx
@@ -0,0 +1,8 @@
+
+export const KakaoIcon = () => {
+  return (
+    <button className="bg-yellow-400 p-5 w-64 border-spacing-1 rounded-xl active:bg-yellow-500">
+      카카오 로그인
+    </button>
+  );
+};
